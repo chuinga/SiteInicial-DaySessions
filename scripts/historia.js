555fe24aa02b06772453ba58304c0a7b61bb1311
Miguel Martins <mfproencamartins@gmail.com>
2024-07-18 20:19:59 +0100

    feat: add 4th javascript example

---
+++ scripts/historia.js
@@ -0,0 +1,8 @@
+let div = document.getElementById('origem');
+div.innerHTML = '<h2>Origem</h2>';
+
+document.getElementById('texto').innerHTML = '<p>Pokémon tem cerca de 1015 espécies fictícias de monstros colecionáveis, cada um com designs e habilidades únicas.' +
+    'Concebido por Satoshi Tajiri no início de 1989, os Pokémon são criaturas que habitam o mundo fictício Pokémon.' +
+    'Os projetos para a multiplicidade de espécies podem inspirar-se em qualquer coisa como animais, plantas, criaturas mitológicas e até objetos inanimados.' +
+    'Muitos Pokémon são capazes de evoluir para espécies mais poderosas, enquanto outros podem sofrer mudanças de forma e obter resultados semelhantes. Existem até alguns deles que não podem evoluir. Originalmente, apenas um punhado de artistas liderados por Ken Sugimori projetou Pokémon.' +
+    'No entanto, até 2013, uma equipe de 20 artistas trabalhou junta para criar novos desenhos de espécies. Sugimori e Hironobu Yoshida lideram a equipe e determinam os projetos finais.</p>';
