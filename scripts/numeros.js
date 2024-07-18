ed0157f43360070ba0c30bb9ba95b259f59ac1c1
Miguel Martins <mfproencamartins@gmail.com>
2024-07-18 21:55:14 +0100

    feat: add 5th javascript example

---
+++ scripts/numeros.js
@@ -0,0 +1,31 @@
+const atualizaContagem = (elemento) => {
+    // Obter o valor a partir do atributo 'data-value' do elemento
+    const valor = parseInt(elemento.dataset.value); // parseInt --> produz um valor inteiro
+    
+    // Calcula o incremento a ser adicionado a cada intervalo de tempo
+    const incremento = Math.ceil(valor / 1000); // Math.ceil arredonda valor para cima
+    
+    // Inicializar o valor inicial da contagem
+    let valorInicial = 0;
+    
+    // Definição de uma função que será executada repetidamente a cada milissegundo
+    const aumentaContagem = setInterval(() => {
+        valorInicial += incremento; // Adiciona o incremento ao valor inicial
+        
+        if (valorInicial > valor) { // Verifica se o valor inicial ultrapassou o valor desejado
+            elemento.textContent = `${valor}`; // Define o texto do elemento para o valor desejado
+            clearInterval(aumentaContagem); // Para a execução repetida da função
+            return;
+        }
+        
+        elemento.textContent = `${valorInicial}`; // Atualiza o texto do elemento para o valor inicial
+    }, 20); // O intervalo de tempo é definido como 1 milissegundo
+};
+
+// Seleciona todos os elementos da página com a classe 'numero' e converte-os num array
+const items = [...document.querySelectorAll('.numero')];
+
+// Para cada elemento selecionado, aplica a função 'atualizaContagem'
+items.forEach((item) => {
+    atualizaContagem(item);
+});
