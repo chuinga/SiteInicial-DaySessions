fa0b3ea2b4ec18ec11b92eae506b108e82be1045
chuinga <mfproencamartins@gmail.com>
2024-07-22 17:48:57 +0100

    update to 9th version

--- scripts/historia.js
+++ scripts/historia.js
@@ -1,12 +1,9 @@
+//Enviar código para o browser
 let div = document.getElementById('origem');
-div.innerHTML = '<h2>Origem</h2>';
+div.innerHTML = "<h2>A origem</h2>";
+/*document.getElementById é um método que permite selecionar elementos 
+com base no valor do atributo id. */
+/*innerHTML é um método que permite obter ou definir o conteúdo HTML de um elemento.*/
 
-document.getElementById('texto').innerHTML = '<p>Pokémon tem cerca de 1015 espécies fictícias de monstros' + 
-    ' colecionáveis, cada um com designs e habilidades únicas. Concebido por Satoshi Tajiri no início de 1989,' + 
-    ' os Pokémon são criaturas que habitam o mundo fictício Pokémon. Os projetos para a multiplicidade de' + 
-    ' espécies podem inspirar-se em qualquer coisa como animais, plantas, criaturas mitológicas e até objetos' + 
-    ' inanimados. Muitos Pokémon são capazes de evoluir para espécies mais poderosas, enquanto outros podem' + 
-    ' sofrer mudanças de forma e obter resultados semelhantes. Existem até alguns deles que não podem evoluir.' + 
-    ' Originalmente, apenas um punhado de artistas liderados por Ken Sugimori projetou Pokémon. No entanto, até' + 
-    ' 2013, uma equipe de 20 artistas trabalhou junta para criar novos desenhos de espécies. Sugimori e Hironobu' + 
-    ' Yoshida lideram a equipe e determinam os projetos finais.</p>';
+//texto
+document.getElementById('texto').innerHTML = '<p>Pokémon tem cerca de 1015 espécies fictícias de monstros colecionáveis, cada um com designs e habilidades únicas. </p><p>Concebido por <em>Satoshi Tajiri</em> no início de 1989, os Pokémon são criaturas que habitam o mundo fictício Pokémon. </p><p>Os projetos para a multiplicidade de espécies podem inspirar-se em qualquer coisa como animais, plantas, criaturas mitológicas e até objetos inanimados. </p><p>Muitos Pokémon são capazes de evoluir para espécies mais poderosas, enquanto outros podem sofrer mudanças de forma e obter resultados semelhantes. Existem até alguns deles que não podem evoluir. Originalmente, apenas um punhado de artistas liderados por Ken Sugimori que projetou Pokémon. </p><p>No entanto, até 2013, uma equipa de 20 artistas trabalharam juntos para criar novos desenhos de espécies. Sugimori e Hironobu Yoshida lideram a equipa e determinam os projetos finais. </p>';
