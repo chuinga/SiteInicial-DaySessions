14864f43f29f7f2b437226d5ce87464a10802e34
chuinga <mfproencamartins@gmail.com>
2024-07-23 17:10:05 +0100

    update to 10th session

---
+++ scripts/galeria.js
@@ -0,0 +1,9 @@
+//galeria da página galeria.html
+
+function myFunction(imgs){
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
